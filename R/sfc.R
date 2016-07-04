34f5ad9fee86a2e6411f792dc1b65dc035f58dcc
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2016-07-04 22:09:08 +0200

    remove package

--- R/sfc.R
+++ R/sfc.R
@@ -1,30 +1,29 @@
-
-bbox.Mtrx = function(x) {
-	mn = apply(x, 2, min)
-	mx = apply(x, 2, max)
+bbox.Mtrx = function(obj) {
+	mn = apply(obj, 2, min)
+	mx = apply(obj, 2, max)
 	c(xmin = mn[1], xmax = mx[1], ymin = mn[2], ymax = mx[2])
 }
-bbox.MtrxSet = function(x) {
-	s = sapply(x, bbox.Mtrx)
+bbox.MtrxSet = function(obj) {
+	s = sapply(obj, bbox.Mtrx)
 	c(xmin = min(s[1,]), xmax = max(s[2,]), ymin = min(s[3,]), ymax = max(s[4,]))
 }
-bbox.MtrxSetSet = function(x) {
-	s = sapply(x, bbox.MtrxSet)
+bbox.MtrxSetSet = function(obj) {
+	s = sapply(obj, bbox.MtrxSet)
 	c(xmin = min(s[1,]), xmax = max(s[2,]), ymin = min(s[3,]), ymax = max(s[4,]))
 }
-bbox.MtrxSetSetSet = function(x) {
-	s = sapply(x, bbox.MtrxSetSet)
+bbox.MtrxSetSetSet = function(obj) {
+	s = sapply(obj, bbox.MtrxSetSet)
 	c(xmin = min(s[1,]), xmax = max(s[2,]), ymin = min(s[3,]), ymax = max(s[4,]))
 }
 #' Return bounding of a simple feature or simple feature set
 #'
 #' Return bounding of a simple feature or simple feature set
-#' @param x object to compute the bounding box from
+#' @param obj object to compute the bounding box from
 #' @export
-bbox = function(x) UseMethod("bbox")
+bbox = function(obj) UseMethod("bbox")
 
 #' @export
-bbox.POINT = function(x) c(xmin = x[1], xmax = x[1], ymin = x[2], ymax = x[2])
+bbox.POINT = function(obj) c(xmin = obj[1], xmax = obj[1], ymin = obj[2], ymax = obj[2])
 #' @export
 bbox.MULTIPOINT = bbox.Mtrx
 #' @export
@@ -36,25 +35,25 @@ bbox.MULTILINESTRING = bbox.MtrxSet
 #' @export
 bbox.MULTIPOLYGON = bbox.MtrxSetSet
 #' @export
-bbox.GEOMETRYCOLLECTION = function(x) {
-	s = sapply(x, bbox) # dispatch on class
+bbox.GEOMETRYCOLLECTION = function(obj) {
+	s = sapply(obj, bbox) # dispatch on class
 	c(xmin = min(s[1,]), xmax = max(s[2,]), ymin = min(s[3,]), ymax = max(s[4,]))
 }
 
 #' @export
-bbox.sfc = function(x) {
-	switch(attr(x, "type"),
-		"POINT" = , "POINT Z" = , "POINT M" = , "POINT ZM" = bbox.Mtrx(do.call(rbind, x)),
-		"MULTIPOINT" = , "MULTIPOINT Z" = , "MULTIPOINT M" = , "MULTIPOINT ZM" = bbox.MtrxSet(x),
-		"LINESTRING" = , "LINESTRING Z" = , "LINESTRING M" = , "LINESTRING ZM" = bbox.MtrxSet(x),
-		"POLYGON" = , "POLYGON Z" = , "POLYGON M" = , "POLYGON ZM" = bbox.MtrxSetSet(x),
+bbox.sfc = function(obj) {
+	switch(attr(obj, "type"),
+		"POINT" = , "POINT Z" = , "POINT M" = , "POINT ZM" = bbox.Mtrx(do.call(rbind, obj)),
+		"MULTIPOINT" = , "MULTIPOINT Z" = , "MULTIPOINT M" = , "MULTIPOINT ZM" = bbox.MtrxSet(obj),
+		"LINESTRING" = , "LINESTRING Z" = , "LINESTRING M" = , "LINESTRING ZM" = bbox.MtrxSet(obj),
+		"POLYGON" = , "POLYGON Z" = , "POLYGON M" = , "POLYGON ZM" = bbox.MtrxSetSet(obj),
 		"MULTILINESTRING" = , "MULTILINESTRING Z" = , "MULTILINESTRING M" = , 
-			"MULTILINESTRING ZM" = bbox.MtrxSetSet(x),
+			"MULTILINESTRING ZM" = bbox.MtrxSetSet(obj),
 		"MULTIPOLYGON" = , "MULTIPOLYGON Z" = , "MULTIPOLYGON M" = , 
-			"MULTIPOLYGON ZM" = bbox.MtrxSetSetSet(x),
+			"MULTIPOLYGON ZM" = bbox.MtrxSetSetSet(obj),
 		"GEOMETRYCOLLECTION" = , "GEOMETRYCOLLECTION Z" = , "GEOMETRYCOLLECTION M" = , 
 			"GEOMETRYCOLLECTION ZM" = { 
-				s = sapply(x, bbox)
+				s = sapply(obj, bbox)
 				c(xmin = min(s[1,]), xmax = max(s[2,]), ymin = min(s[3,]), ymax = max(s[4,]))
 			},
 		stop("simple feature type not supported")
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# sf: simple features for R
+# Simple Features for R
 
-## [![Build Status](https://travis-ci.org/edzer/sf.png?branch=master)](https://travis-ci.org/edzer/sf) [![License](http://img.shields.io/badge/license-GPL%20%28%3E=%202%29-brightgreen.svg?style=flat)](http://www.gnu.org/licenses/gpl-2.0.html) [![CRAN](http://www.r-pkg.org/badges/version/sf)](http://cran.rstudio.com/package=sf) [![Downloads](http://cranlogs.r-pkg.org/badges/sf?color=brightgreen)](http://www.r-pkg.org/pkg/sf)
+(defunct; repository has moved [here](https://github.com/edzer/sfr))
+
